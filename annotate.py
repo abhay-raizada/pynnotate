6cf9ef3dee49a52c8d0d82425ad5a4163c79cbf9
Abhay Raizada <toabhayraizada@gmail.com>
2019-03-12 18:48:35 +0530

    Add some tests

--- annotate.py
+++ annotate.py
@@ -1,4 +1,15 @@
 import os
-
+from functools import  partial
 def models(model_path = 'models'):
-  return os.listdir(model_path)
+  partial_join = partial(os.path.join, model_path)
+  return list(map(os.path.abspath,
+    list(map(partial_join, os.listdir(model_path)))))
+
+def main():
+  for model_path in models:
+    model_dir, model_file = os.path.split(model_path)
+    model_class = class_from_filename(model_file)
+
+def class_from_filename(model_file):
+  return os.path.splitext(
+    model_file)[0].replace('_', ' ').title().replace(' ', '')
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,9 +1,16 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
-from annotate import models
+from annotate import models, class_from_filename
 
 def test_models():
   result = models('tests/fixture_models')
   result.sort()
+  result = list(map( lambda x: os.path.split(x)[1], result))
   assert result == ['fixture_model_1.py', 'fixture_model_2.py']
+
+def test_class_from_filename():
+  assert class_from_filename('class_name.py') == 'ClassName'
+
+def test_class_from_filename_multiple():
+  assert class_from_filename('class_name_sfsaa.py') == 'ClassNameSfsaa'
