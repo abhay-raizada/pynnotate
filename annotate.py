147a4c8972df8b3f0220384e193dab7b46a770e4
Abhay Raizada <toabhayraizada@gmail.com>
2019-03-13 00:39:07 +0530

    Finish first prototype

--- annotate.py
+++ annotate.py
@@ -1,15 +1,54 @@
 import os
 from functools import  partial
+from orator import DatabaseManager
+import yaml
+
 def models(model_path = 'models'):
-  partial_join = partial(os.path.join, model_path)
-  return list(map(os.path.abspath,
+    partial_join = partial(os.path.join, model_path)
+    return list(map(os.path.abspath,
     list(map(partial_join, os.listdir(model_path)))))
 
-def main():
-  for model_path in models:
-    model_dir, model_file = os.path.split(model_path)
-    model_class = class_from_filename(model_file)
+def get_model_data():
+    db = DatabaseManager(get_config_file())
+    schema_manager = db.get_schema_manager()
+    file_info_map = {}
+    for model_path in models():
+        _, model_file = os.path.split(model_path)
+        table_name = table_name_from_filename(model_file)
+        columns_description = get_column_description_from_object(schema_manager, table_name)
+        indices_description = get_indices_description_from_oject(schema_manager, table_name)
+        file_info_map[model_path] = {
+            'columns': columns_description,
+            'indices': indices_description
+        }
+    return file_info_map
+
+def get_indices_description_from_oject(schema_manager, table_name):
+    indices_description = {}
+    for index_name, index_obj in schema_manager.list_table_indexes(table_name):
+        indices_description[index_name] = {
+            'columns': index_obj.get_unquoted_columns(),
+            'is_unique?': index_obj.s_unique(),
+            'is_primary?': index_obj.is_primary()
+        }
+    return indices_description
+
+def  get_column_description_from_object(schema_manager, table_name):
+    columns_description = {}
+    for column_name, column_obj in schema_manager.list_table_columns(table_name):
+        columns_description[column_name] = column_obj.to_dict()
+    return columns_description
+
+def get_config_file(config_path='orator.py'):
+    config = {}
+    exec(open(config_path).read(), {}, config)
+    return config
+
+def table_name_from_filename(model_file):
+    return os.path.splitext(
+    model_file)[0]
 
 def class_from_filename(model_file):
-  return os.path.splitext(
+    return os.path.splitext(
     model_file)[0].replace('_', ' ').title().replace(' ', '')
+}
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,8 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
-from annotate import models, class_from_filename
+from annotate import models, class_from_filename, table_name_from_filename, get_config_file
+import tempfile
 
 def test_models():
   result = models('tests/fixture_models')
@@ -14,3 +15,9 @@ def test_class_from_filename():
 
 def test_class_from_filename_multiple():
   assert class_from_filename('class_name_sfsaa.py') == 'ClassNameSfsaa'
+
+def test_table_name_from_filename():
+  assert table_name_from_filename('engine_model_names.py') == 'engine_model_names'
+
+def test_get_config_file():
+  assert get_config_file('tests/fixture_models/fixture_model_1.py') == {'DATABASES': { 'a': 1, 'b': 2 } }
