e0c5a3e5e18c9efc68503f961809cd976d6b283c
Abhay Raizada <toabhayraizada@gmail.com>
2019-03-24 14:08:51 +0530

    Pytest: freeze version on travis

--- tests/test_annotate.py
+++ tests/test_annotate.py
@@ -1,6 +1,7 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+
 from annotate import write_to_file
 
 from lib.settings import get_config_file
