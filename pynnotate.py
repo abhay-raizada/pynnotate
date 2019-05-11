1621b507c8c53e51c5bcf02ef8019122a1b5dc01
Abhay Raizada <toabhayraizada@gmail.com>
2019-05-11 22:28:06 +0530

    Setup: Use find_packages instead of get_packages
    Fixes https://github.com/abhsag24/pynnotate/issues/15

--- pynnotate.py
+++ pynnotate.py
@@ -11,11 +11,11 @@ from annotate import write_to_file
 @click.option('--db-user')
 @click.option('--db-password')
 @click.option('--config-path')
-def main(model_path, db_name, db, db_host, db_user, db_password, config_path):
-    print(model_path, db_name, db)
+def main(model_path, db, db_name, db_host, db_user, db_password, config_path):
     write_to_file(model_path, config_path,
         db_name=db_name, db_host=db_host,
-        db_user=db_user, db_password=db_password, db=db)
+        db_user=db_user, db_password=db_password, db=db
+    )
 
 if __name__ == '__main__':
     main()
